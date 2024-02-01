05a9d2e0898af33b27fe0480cb545ebc3472f6ce
Lance <lancerprogram@gmail.com>
2024-01-31 17:44:30 -0800

    Add some basic limited A24 movies

---
+++ data.py
@@ -0,0 +1,15 @@
+# Define a small sample dataset of A24 movies categorized by genre and mood
+a24_movies = {
+    'Horror': {
+        'Eerie': ['The Witch', 'Hereditary'],
+        'Suspenseful': ['It Comes at Night', 'Green Room']
+    },
+    'Drama': {
+        'Emotional': ['Moonlight', 'Room'],
+        'Thought-provoking': ['The Farewell', 'A Ghost Story']
+    },
+    'Comedy': {
+        'Quirky': ['Lady Bird', 'The Disaster Artist'],
+        'Dark': ['Ingrid Goes West', 'The Lobster']
+    }
+}
